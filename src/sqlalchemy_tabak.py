77b4167496a6984a15f4570ee03da3210c505397
Evgeny011 <ptua785@gmail.com>
2024-01-13 18:50:23 +0300

    .

--- src/sqlalchemy_tabak.py
+++ src/sqlalchemy_tabak.py
@@ -1,13 +1,15 @@
 import sqlalchemy as db
 
 
-engine = db.create_engine('sqlite:///Inventories-sqlalchemy.db', echo=True)
+engine = db.create_engine('sqlite:///Inventories-sqlalchemy.db', 
+                          echo=True)
 connection = engine.connect()
 metadata = db.MetaData()
 Inventories = db.Table('Inventories', metadata,
 db.Column("ID", db.Integer),
-db.Column("From", db.Date),
-db.Column("To", db.Date),
+db.Column("Start_Date", db.Date),
+db.Column("End_Date", db.Date),
 db.Column("Timestamp", db.TIMESTAMP)
 )
 metadata.create_all(engine)
+
