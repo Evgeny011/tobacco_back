1e3b65637719081a331e541afbf045035582511d
Evgeny011 <ptua785@gmail.com>
2024-01-19 13:49:03 +0300

    fix allow origins

--- src/fastapi_tabak.py
+++ src/fastapi_tabak.py
@@ -4,6 +4,8 @@ from src.queries.inventory_router import inventory_router
 
 import src.sqlalchemy_tabak
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 ALLOW_ORIGINS = ["*"]
 
@@ -11,4 +13,8 @@ app = FastAPI(
     title= 'Inventory calculator'
 )
 app.include_router(inventory_router)
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=ALLOW_ORIGINS, allow_credentials=True
+)
 
